f9f4fa7c561417dd76d9c7725c37412eefe1c63d
Bob <bob.e.baxter@gmail.com>
2019-06-29 12:06:35 -0500

    auth works

--- src/components/StaffRoom/StaffRoom.js
+++ src/components/StaffRoom/StaffRoom.js
@@ -2,16 +2,27 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import employeeShape from '../../helpers/propz/employeeShape';
 import './StaffRoom.scss';
+
 import Employee from '../Employee/Employee';
+import employeeData from '../../App/employees';
 
 class StaffRoom extends React.Component {
   static propTypes = {
     employees: PropTypes.arrayOf(employeeShape.employeeShape),
   }
 
+  state = {
+    employees: [],
+  }
+
+  componentDidMount() {
+    this.setState({
+      employees: employeeData,
+    });
+  }
+
   render() {
-    const { employees } = this.props;
-    const makeEmployees = employees.map(employee => (
+    const makeEmployees = this.state.employees.map(employee => (
       <Employee key={employee.id} employee={employee} />
     ));
 
